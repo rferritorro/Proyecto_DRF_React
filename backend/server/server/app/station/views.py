a78a9fed231d463b54b6a545b99ee192443faf0e
rferritorro <rferritorro@gmail.com>
2023-02-07 16:31:58 +0100

    Register has been created

--- backend/server/server/app/station/views.py
+++ backend/server/server/app/station/views.py
@@ -10,17 +10,10 @@ class StationView(viewsets.GenericViewSet):
         qs = Station.objects.raw(sql)
         qs_json = StationSerializer(qs, many=True).data
         return Response(qs_json, content_type='application/json')
-    def GetStationByID(self, request, id):
-        sql = 'SELECT station.id, station.name, station.lat, station.long, station.img, slots.bikes FROM station LEFT JOIN slots ON slots.station_id = station.id WHERE station.id='+ id +' ORDER BY id ASC'
-        qs = Station.objects.raw(sql)
-        qs_json = StationSerializer(qs, many=True).data
-        return Response(qs_json, content_type='application/json')
+
     def CreateStation(self, request):
         serializer_data = request.data
         StationSerializer.CreateStation(data=serializer_data)
-        # serializer = self.serializer_class(
-        #     data = serializer_data
-        # )
         return Response({"ADDED NEW STATION SUCCESFULL"}, status=status.HTTP_201_CREATED)
     def UpdateStation(self, request):
         serializer_data = request.data
--- backend/server/server/app/user/serializers.py
+++ backend/server/server/app/user/serializers.py
@@ -2,8 +2,9 @@ from pyexpat import model
 from rest_framework import serializers
 from .models import User
 from ..profile.models import Profile
-import json
+from ..profile.serializers import ProfileSerializer
 from django.core.serializers import serialize
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,11 +19,21 @@ class UserSerializer(serializers.ModelSerializer):
         }
     
     def create(context):
-        profile = Profile.objects.get(id = context['profile_id'])
-        user = User.objects.create(
-            username = context['username'],
-            password = context['password'],
-            profile = profile
-        )
-        serialized_user = UserSerializer.to_user(user)
-        return serialized_user
+        # Create first profile to redirect with user
+        user_exist=User.objects.filter(username=context['username']).exists()
+        if user_exist:
+            raise serializers.ValidationError("User '{}' has already existed".format(context['username']))
+        else:
+            # email create here
+            new_profile = Profile.objects.create(
+                avatar = context['avatar']
+            )
+            ProfileSerializer.to_profile(new_profile)
+            profile = Profile.objects.get(id = new_profile.id)
+            user = User.objects.create(
+                username = context['username'],
+                password = make_password(context['password']),
+                profile = profile
+            )
+            serialized_user = UserSerializer.to_user(user)
+            return serialized_user
