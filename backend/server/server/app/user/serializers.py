38bc24873d59abf6be42ef5e8acd657b0645970f
rferritorro <rferritorro@gmail.com>
2023-02-14 15:36:48 +0100

    Profile resolve errors

--- backend/server/server/app/user/serializers.py
+++ backend/server/server/app/user/serializers.py
@@ -47,6 +47,11 @@ class UserSerializer(serializers.ModelSerializer):
             serialized_user = UserSerializer.to_user(user)
             return serialized_user
 
+    def getUser(context):
+        user = User.objects.get(id = context["id"])
+        serialized_user = UserSerializer.to_user(user)
+        return serialized_user
+        
     def loginSerializer(context):
         password = context['password']
         user = get_object_or_404(User, username=context['username'])
